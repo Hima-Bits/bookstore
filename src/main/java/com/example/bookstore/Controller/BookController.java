af95fc2900c937fb6bf416de3cf196e9bbde1dfc
Hima V S <hima.v.s@sap.com>
2022-06-02 10:02:16 +0530

    Api's for Book Store

---
+++ src/main/java/com/example/bookstore/Controller/BookController.java
@@ -0,0 +1,119 @@
+package com.example.bookstore.Controller;
+
+import com.example.bookstore.Model.*;
+import com.example.bookstore.Repository.Repository;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/BookStore")
+public class BookController {
+    private final String API_KEY="123";
+    private Repository reso;
+    {
+        reso=new Repository();
+
+    }
+    @PostMapping("/create")
+    public Result create(@RequestBody UpdateModel updateModel) {
+System.out.println(updateModel);
+        if(updateModel.getKey()!=null && updateModel.getKey().equals(API_KEY))
+            return reso.create(updateModel.getBook());
+        else if(updateModel.getKey()==null)
+            return reso.getResult("Failure "," API key is not sent");
+        else {
+            return reso.getResult("Failure "," API key is invalid");
+        }
+
+    }
+
+
+
+
+    @GetMapping("/getAllBooks")
+    public Result getAllBooks() {
+
+        return reso.getAllBooks();
+    }
+
+    @PostMapping("/getBook")
+    public Result getBook(@RequestBody AuthModel auth) {
+
+        return reso.getBook(auth.getId());
+    }
+
+    @PostMapping("/getBooksByName")
+    public Result getBooksByName(@RequestBody AuthModel auth) {
+        return reso.getBookbyName(auth.getBookName());
+    }
+
+
+    @DeleteMapping("/deleteBook")
+    public Result deleteBook(@RequestBody AuthModel auth) {
+        try{
+            if(auth.getKey()!=null && auth.getKey().equals(API_KEY))
+                return reso.deleteBook(auth.getId());
+            else if(auth.getKey()==null)
+                return reso.getResult("Failure "," API key is not sent");
+            else {
+                return reso.getResult("Failure "," API key is invalid");
+            }
+        }
+        catch(Exception e){
+            return reso.getResult("Failure "," Something happened w");
+
+        }
+    }
+
+
+    @PutMapping("/update2")
+    public Result updateBook(@RequestBody updateBook book) {
+        try{
+            Book b=new Book();
+            b.setId(Long.parseLong(book.getBookId()));
+            b.setBookName(book.getBookName());
+            b.setAuthorName(book.getAuthorName());
+            b.setDescription(book.getDescription());
+            b.setPrice(book.getPrice());
+            b.setPublisher(book.getPublisher());
+            System.out.println("cont+"+book);
+            System.out.println("b:"+b);
+            if(book.getBookId()!=null)
+                return reso.updateBook(b);
+            else {
+                return reso.getResult("Failure "," Book not found");
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return reso.getResult("Failure "," Something unexpected happened");
+        }
+    }
+
+    @PutMapping("/update")
+    public Result updateBook2(@RequestBody UpdateModel updateModel) {
+        try{
+//            Book b=new Book();
+//            b.setId(Long.parseLong(book.getBookId()));
+//            b.setBookName(book.getBookName());
+//            b.setAuthorName(book.getAuthorName());
+//            b.setDescription(book.getDescription());
+//            b.setPrice(book.getPrice());
+//            b.setPublisher(book.getPublisher());
+//            System.out.println("cont+"+book);
+//            System.out.println("b:"+b);
+            if(updateModel.getKey()!=null && updateModel.getKey().equals(API_KEY))
+                return reso.updateBook(updateModel.getBook());
+            else {
+                return reso.getResult("Failure "," Book not found");
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return reso.getResult("Failure "," Something unexpected happened");
+        }
+    }
+
+
+}
---
+++ src/main/java/com/example/bookstore/Repository/Repository.java
@@ -0,0 +1,83 @@
+package com.example.bookstore.Repository;
+
+import com.example.bookstore.DAO.DAO;
+import com.example.bookstore.Model.Book;
+import com.example.bookstore.Model.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Repository {
+private DAO dao;
+{
+	dao=new DAO();
+}
+	public Result create(Book book) {
+		long id=getTimeStamp();
+		book=new Book(book,id);
+		dao.save(book);
+		List<Book> list=new ArrayList<Book>();
+		list.add(book);
+		return getResult(list);
+	}
+	public Result getAllBooks(){
+		return  getResult(dao.getAll());
+	}
+	public Result getBook(long id) {
+		Book b=dao.getBook(id);
+
+		List<Book> list=new ArrayList<Book>();
+		if(b!=null)
+		list.add(b);
+		return getResult(list);
+	}
+	public Result getBookbyName(String name) {
+		if(name!=null)
+		return getResult(dao.getBookByName(name));
+		else {
+			return getResult("failure","please provide name of book");
+		}
+	}
+	public Result deleteBook(long id) {
+		Book b=dao.deleteBook(id);
+		List<Book> list=new ArrayList<Book>();
+		if(b!=null)
+		list.add(b);
+		return getResult(list);
+	}
+	
+	public Result updateBook(Book book) {
+		System.out.println("Book in repo"+book);
+		Book b=dao.UpadteBook(book);
+		List<Book> list=new ArrayList<Book>();
+		if(b!=null) {
+			list.add(b);
+		}
+		return getResult(list);
+	}
+	
+	private long getTimeStamp(){
+		return System.currentTimeMillis();
+	}
+	private Result getResult(List<Book> list) {
+		Result l=new Result();
+		if(list.size()>0) {
+		l.setResult("success");
+		l.setStatus("OK");
+		}
+		else {
+			l.setResult("failure");
+			l.setStatus("No book found");
+			return l;
+		}
+		l.getMap().put("data", list);
+		return l;
+	}
+	public Result getResult(String result,String status) {
+		Result l=new Result();
+		l.setResult(result);
+		l.setStatus(status);
+		return l;
+	}
+}
